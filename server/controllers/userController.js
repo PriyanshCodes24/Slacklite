9c3a35a4cf3cb895060d117964e49e6c2f012f7f
Priyansh <prinshu.patel2004@gmail.com>
2026-05-03 13:58:40 +0530

    added searchIsers in backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 
-const getAllUsers = async (req, res) => {
+exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find({ _id: { $ne: req.user._id } }).select(
       "_id name email",
@@ -12,4 +12,21 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-module.exports = { getAllUsers };
+exports.searchUsers = async (req, res) => {
+  try {
+    const query = req.query.q;
+
+    if (!query) return res.json([]);
+
+    const users = await User.find({
+      name: { $regex: query, $options: "i" },
+      _id: { $ne: req.user._id },
+    }).select("name email");
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ msg: "Search failed" });
+  }
+};
+
+// module.exports = { getAllUsers };
