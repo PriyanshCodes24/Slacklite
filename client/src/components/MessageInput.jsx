11b294ea15bd0094b7c0e27c4c3e9e55c19e1bc5
Priyansh <prinshu.patel2004@gmail.com>
2026-06-09 14:43:06 +0530

    improved expanding buttons in input

--- client/src/components/MessageInput.jsx
+++ client/src/components/MessageInput.jsx
@@ -113,7 +113,7 @@ export const MessageInput = ({
     }
   };
   return (
-    <div className="flex mt-3 gap-2">
+    <div className="flex items-end mt-3 gap-2">
       <input
         type="file"
         accept="image/*"
@@ -128,18 +128,18 @@ export const MessageInput = ({
         value={message}
         onChange={handleInputChange}
         placeholder="Type message..."
-        className="flex-1 bg-gray-800 border border-gray-600 py-2 px-3 rounded text-white placeholder-gray-400 resize-none overflow-y-auto min-h-11 max-h-40 leading-normal"
+        className="flex-1 bg-gray-800 border border-gray-600 py-2 px-3 rounded-2xl text-white placeholder-gray-400 resize-none overflow-y-auto min-h-11 max-h-40 leading-normal outline-none focus:border-blue-500"
         onKeyDown={handleEnter}
       />
       <button
         type="button"
         onClick={() => fileInputRef.current?.click()}
-        className="bg-gray-800 hover:bg-gray-600 cursor-pointer px-3 rouded"
+        className="h-11 w-11 shrink-0 bg-gray-800 hover:bg-gray-700 flex items-center justify-center rounded cursor-pointer "
       >
         <LuPaperclip />
       </button>
       <button
-        className="bg-blue-600 hover:bg-blue-700 px-4 rounded cursor-pointer text-white"
+        className="h-11 w-11 shrink-0 bg-blue-600 hover:bg-blue-700 flex items-center justify-center rounded cursor-pointer "
         onClick={sendMessage}
       >
         <BsFillSendFill />
